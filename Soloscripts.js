548c7aa19e0d59ac749d84d27560ee25eae1dbf7
lizpiet <lizziepowers55@gmail.com>
2015-08-20 08:07:11 -0500

    Update Soloscripts.js

--- Soloscripts.js
+++ Soloscripts.js
@@ -1,4 +1,5 @@
 
+//QUESTION 1
 
 		var primes = [2, 3, 5, 7, 11, 13, 17, 19];
 		var nonPrimes = [1, 4, 6, 8, 9, 10, 12, 14];
@@ -13,4 +14,25 @@ var combinedFunction = function(){								//Create new function
 
 
 combinedFunction();												// return function
-console.log(combined);											//console.log to check work
+console.log(combined);	//console.log to check work
+
+//QUESTION 2
+
+
+$(document).ready(function(){
+
+var numbers1 = [4, 1, 6, 5, 8];
+var numbers2 = [7, 3, 2, 9, 10];
+var numbers3 = [];
+
+var numbers3 = $.merge(  numbers1 ,  numbers2  )				 //merge in jQUery
+
+ function sortedArray(a,b){							// comparator function
+return a - b;										//because values get sorted lexiconically,
+													// 10 will end up in a funky spot
+}
+numbers3.sort(sortedArray);							// call function
+
+console.log(numbers3);								// console.log to check work
+
+});
